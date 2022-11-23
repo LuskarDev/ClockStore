a7207f34c2ad5fb51306f4295116eed032ba2348
Lucas Oliveira <87589466+LuskarDev@users.noreply.github.com>
2022-11-23 01:26:49 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const account = document.querySelector(".accounts"),
+IconAccounts = document.querySelector(".account")
+
+account.onclick = () => {
+    IconAccounts.classList.toggle("showOptions")
+} 
+
