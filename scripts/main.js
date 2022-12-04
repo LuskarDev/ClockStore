58eedccc01440af0cc226d97c25ade15c6ee4385
Lucas Oliveira <87589466+LuskarDev@users.noreply.github.com>
2022-12-03 22:01:59 -0300

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-const account = document.querySelector(".accounts"),
-IconAccounts = document.querySelector(".account")
-
-account.onclick = () => {
-    IconAccounts.classList.toggle("showOptions")
-} 
-
